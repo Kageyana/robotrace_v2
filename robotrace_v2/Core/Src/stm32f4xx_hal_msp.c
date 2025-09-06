9ef163c2f4f254200f3a26644ee5de81ee538304
Kageyana <uc.aw.ac.ce@gmail.com>
2025-09-06 17:23:13 +0900

    I2CのDMAをCircularモードにした

--- robotrace_v2/Core/Src/stm32f4xx_hal_msp.c
+++ robotrace_v2/Core/Src/stm32f4xx_hal_msp.c
@@ -320,7 +320,7 @@ void HAL_I2C_MspInit(I2C_HandleTypeDef* hi2c)
     hdma_i2c3_tx.Init.MemInc = DMA_MINC_ENABLE;
     hdma_i2c3_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
     hdma_i2c3_tx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
-    hdma_i2c3_tx.Init.Mode = DMA_NORMAL;
+    hdma_i2c3_tx.Init.Mode = DMA_CIRCULAR;
     hdma_i2c3_tx.Init.Priority = DMA_PRIORITY_LOW;
     hdma_i2c3_tx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
     if (HAL_DMA_Init(&hdma_i2c3_tx) != HAL_OK)
