f6fc702529cad865d812f15f1c75f6ee7b720983
Kageyama <uc.aw.ac.ce@gmail.com>
2025-09-07 03:00:12 +0900

    MOTOR_AD_WINDOWのチェックにコメント追加

--- robotrace_v2/Core/Inc/motor.h
+++ robotrace_v2/Core/Inc/motor.h
@@ -20,6 +20,11 @@
 #define RREF_R 1940.0F // 抵抗値
 
 #define MOTOR_AD_WINDOW 64
+// MOTOR_AD_WINDOW は2の冪である必要があるため、変更時は注意
+// ビット演算で2の冪かを静的に検証し、条件を満たさない場合はコンパイルエラー
+#if (MOTOR_AD_WINDOW & (MOTOR_AD_WINDOW - 1)) != 0
+#error "MOTOR_AD_WINDOW must be a power of two"
+#endif
 //====================================//
 // グローバル変数の宣言
 //====================================//
